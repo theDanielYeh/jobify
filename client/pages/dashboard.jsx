8a201b2f3b74922432d7a8da04a3386f1408c67f
Daniel Yeh <dyeh1989@gmail.com>
2022-06-04 06:12:49 +0000

    issue #2: enabled signout feature

--- client/pages/dashboard.jsx
+++ client/pages/dashboard.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import { Navbar, Container, Nav } from 'react-bootstrap';
+import AppContext from '../lib/app-context';
 
 export default class Dashboard extends React.Component {
   render() {
+    const { handleSignOut } = this.context;
     return (
-      <div className="">Hello, Dashboard!</div>
+      <Navbar bg="primary" variant="dark">
+        <Container className="dashboard-container">
+          <Navbar.Brand href="#home">Welcome [user]!</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#dashboard">Dashboard</Nav.Link>
+            <Nav.Link href="">
+              <i className="fa-solid fa-square-plus fa-2xl"></i>
+            </Nav.Link>
+            <Nav.Link href="#about">About</Nav.Link>
+            <Nav.Link href="#about" onClick={handleSignOut}>Sign out</Nav.Link>
+          </Nav>
+
+        </Container>
+      </Navbar>
     );
   }
 }
+
+Dashboard.contextType = AppContext;
